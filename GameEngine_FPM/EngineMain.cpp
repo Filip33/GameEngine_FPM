d88b708807557c7ea3a4068d0f46ab4a2bb6c7bb
Filip33 <filip.pjano@gmail.com>
2021-04-20 20:15:25 -0400

    updated main class with while loop to close window

--- GameEngine_FPM/EngineMain.cpp
+++ GameEngine_FPM/EngineMain.cpp
@@ -4,6 +4,20 @@ int main(int argc, char* args[])
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "MyEngine");
 
+	// Run the program as long as the window is open
+	while (window.isOpen() == true)
+	{
+		sf::Event _event;
+
+		while (window.pollEvent(_event) == true)
+		{
+			if (_event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
 	return 0;
 }
 
